2df043c83797bbb13d22593bfd8f057fd764b6b9
Tamara <tamilanovska@gmail.com>
2025-08-27 15:15:12 +0100

    Create custom header for the separator

---
+++ Test2.swift
@@ -0,0 +1,9 @@
+//
+// ----------------------------------
+// Project: FileHeaderTemplateExample
+// Created by: Tamara
+// ----------------------------------
+// Copyright © 2025 Tamara Dev Solutions. All rights reserved.
+
+
+import Foundation
