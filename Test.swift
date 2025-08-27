b808719da558f0bb3f801cba444c0508622c0c05
Tamara <tamilanovska@gmail.com>
2025-08-27 15:05:57 +0100

    Create custom template header and add new test file

---
+++ Test.swift
@@ -0,0 +1,8 @@
+//
+// Project: FileHeaderTemplateExample
+// Created by: Tamara
+// ----------------------------------
+// Copyright © 2025 Tamara Dev Solutions. All rights reserved.
+
+
+import Foundation
