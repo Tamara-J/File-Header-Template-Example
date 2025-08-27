efb6bad97ff610b2d1b758a3d114be4efe3c8d49
Tamara <tamilanovska@gmail.com>
2025-08-27 14:38:36 +0100

    Initial commit

---
+++ FileHeaderTemplateExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FileHeaderTemplateExample
+//
+//  Created by Tamara on 27.8.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FileHeaderTemplateExample/FileHeaderTemplateExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileHeaderTemplateExampleApp.swift
+//  FileHeaderTemplateExample
+//
+//  Created by Tamara on 27.8.25.
+//
+
+import SwiftUI
+
+@main
+struct FileHeaderTemplateExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
